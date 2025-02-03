14222436a9c1048975ce06fb712d017ddaa01440
Francoviera <vierafranco1@gmail.com>
2025-02-03 15:17:20 -0300

    init backend spring boot service

---
+++ service/src/main/java/com/backOffice_electric_cooperative/demo/BackOfficeElectricCooperativeApplication.java
@@ -0,0 +1,13 @@
+package com.backOffice_electric_cooperative.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackOfficeElectricCooperativeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackOfficeElectricCooperativeApplication.class, args);
+	}
+
+}
